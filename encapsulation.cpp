8a8192a97b85c9cc8e4d3bfc3107e4690238d83b
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2021-10-24 22:58:50 -0500

    Encapsulation

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -2,8 +2,27 @@
 #include<string>
 using namespace std; 
 
+class Persona {
+    private: 
+        string nombre; // De esta forma los datos son privados y no puedo editarlos
+        int edad; 
+    public: 
+        
+        Persona(string n, int e): nombre(n),  edad(e) {} // De esta forma tambien puedo crear constructuores
+
+        ~Persona(){
+            cout << "Destructor!" <<endl; 
+
+        }
+        void saludar(){
+            cout << nombre <<endl; 
+        }
+};
 
 int main(){
+    Persona *p = new Persona("Diana", 25); 
+    p->saludar(); 
+
 
     return 0; 
 }
